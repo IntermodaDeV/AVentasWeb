c3b058e0cddea58e017b7753a98a0ea759e3d9c6
Francisco A. Garcia <frangarcia38@hotmail.com>
2020-03-03 15:16:04 -0600

    Commit con los siguientes cambios: -Se crea los componentes CuotaModal y FacturasModal. -Desglose de facturas por el tipo de credito en resumen de cartera.

--- src/components/Recibos/Facturas/FacturaTable.js
+++ src/components/Recibos/Facturas/FacturaTable.js
@@ -1,14 +1,16 @@
-import React, {
-  //  useEffect, useState 
+import React, {useState
 } from 'react'
-import MUIDataTable from 'mui-datatables';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import moment from 'moment';
-import 'moment/locale/es';
+import MUIDataTable from 'mui-datatables'
+import moment from 'moment'
+import 'moment/locale/es'
 import {
   Button,
   // TablePagination, TableCell
 } from '@material-ui/core';
+import { FaEye } from "react-icons/fa";
+import FacturasModal from "components/Recibos/FacturasModal/FacturasModal";
+import FacturasModalPrecompra from "components/Recibos/FacturasModal/FacturasModalPrecompra";
+import CuotaModal from "components/Recibos/FacturasModal/CuotaModal";
 
 moment.locale('es')
 
@@ -66,7 +68,6 @@ const getOptions = (props) => {
     download: false,
     filter: false,
     viewColumns: false,
-    responsive: "scrollMaxHeight",
     search: false,
     // customFooter: (count, page, rowsPerPage, changeRowsPerPage, changePage, textLabels) => CustomFooter(count, page, rowsPerPage, changeRowsPerPage, changePage, textLabels, props),
     customFooter: () => { },
@@ -75,12 +76,87 @@ const getOptions = (props) => {
   return options;
 }
 
+
+/* 
+const CustomFooter = (count, page, rowsPerPage, changeRowsPerPage, changePage, textLabels, props) => {
+
+  const Totales = {
+    Disponible: 0,
+    SaldoTotal: 0,
+    C15Dias: 0,
+  };
+
+  props.Credito.forEach(fact => {
+    Totales.Disponible += fact.Disponible;
+    Totales.SaldoTotal += fact.SaldoTotal;
+    Totales.C15Dias += fact.C15Dias;
+
+  })
+
+
+  const handleRowChange = event => {
+    changeRowsPerPage(event.target.value);
+  };
+
+  const handlePageChange = (_, page) => {
+    changePage(page);
+  };
+
+  return (
+    <tfoot>
+      <tr>
+        <TableCell style={{ width: 'calc((1/5)*100%)' }}>
+          <h6 className="font-weight-bolder text-dark">Totales</h6>
+        </TableCell>
+        <TableCell style={{ width: 'calc((1/5)*100%)', paddingLeft: '0.2%' }} className="font-weight-bolder text-dark">
+          {Totales.Disponible.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+        </TableCell>
+        <TableCell style={{ width: 'calc((1/5)*100%)', paddingLeft: '0.2%' }} className="font-weight-bolder text-dark">
+          {Totales.SaldoTotal.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+        </TableCell>
+        <TableCell style={{ width: 'calc((1/5)*100%)', paddingLeft: '0.2%' }} className="font-weight-bolder text-dark">
+          {Totales.C15Dias.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+        </TableCell>
+        <TableCell style={{ width: 'calc((1/5)*100%)' }}></TableCell>
+      </tr>
+      {false && <tr>
+        <TableCell style={{ justifyContent: 'flex-end', padding: '0px 24px 0px 24px' }} colSpan={1000}>
+          <TablePagination
+            component="div"
+            count={count}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            labelRowsPerPage={textLabels.rowsPerPage}
+            labelDisplayedRows={({ from, to, count }) => `${from}-${to} ${textLabels.displayRows} ${count}`}
+            backIconButtonProps={{
+              'aria-label': textLabels.previous,
+            }}
+            nextIconButtonProps={{
+              'aria-label': textLabels.next,
+            }}
+            rowsPerPageOptions={[10, 20, 100]}
+            onChangePage={handlePageChange}
+            onChangeRowsPerPage={handleRowChange}
+          />
+        </TableCell>
+      </tr>}
+    </tfoot>
+  );
+} */
+
 const FacturaTable = props => {
+  const [DataModal, setDataModal] = useState([]);
+  const [DataModalPrecompra, setDataModalPrecompra] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [openModalPrecompra, setOpenModalPrecompra] = useState(false);
+  const [cuotasa,setCuotasa] = useState([]);
+  const [openModalCuota,setOpenModalCuota] = useState(false);
+
   const setCuotas = (tipoPedido) => {
     const cuotas = props.AcuerdosXTipoPedido.filter(acu => acu.TipoPedido === tipoPedido);
-
     props.SetCuotas(cuotas)
   };
+
   const data = [];
   const Totales = {
     Disponible: 0,
@@ -88,16 +164,175 @@ const FacturaTable = props => {
     C15Dias: 0,
     Vencido: 0
   };
+
+  const handleOpenModalCuota = (cuotas)=>{
+    setCuotasa(cuotas);
+    setOpenModalCuota(true);
+  }
+
+  const ProcesarFactura = (factura,credito,acuerdo)=>{
+      let dias = moment(factura.FechaVencimiento).diff(moment(new Date()), 'days')
+      let diasDescuento = 0;
+      let fechaDescuento = moment(factura.FechaMaxDescuento);
+      if (fechaDescuento.isValid()) {
+        diasDescuento = moment(factura.FechaMaxDescuento).diff(moment(new Date()), 'days');
+      }
+
+      if(credito ==='Precompra'){
+         
+        let cuotasTmp = [];
+
+        if(factura.Cuotas){
+          factura.Cuotas.forEach(cuota=>{
+              cuotasTmp.push(ProcesarCuota(cuota,acuerdo));
+          });
+        }  
+
+        let facturatmp={
+          NumeroFactura: factura.Factura,
+          Dias: dias,
+          DiasDescuento: diasDescuento,
+          Tipo: factura.Tipo,
+          Fecha: moment(factura.FechaFactura).format("DD/MM/YYYY"),
+          Vencimiento: moment(factura.FechaVencimiento).format("DD/MM/YYYY"),
+          FechaDescuento: fechaDescuento.isValid() ? fechaDescuento.format("DD/MM/YYYY") : "",
+          Valor: factura.TotalFactura.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
+          Saldo: factura.Saldo.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
+          Cuotas:(<FaEye size={"20px"} style={{display:"inline-block",marginLeft:'10px'}} onClick={()=>handleOpenModalCuota(cuotasTmp)}/>)
+        }
+        return facturatmp;
+      }
+
+      let facturatmp={
+        NumeroFactura: factura.Factura,
+        Dias: dias,
+        DiasDescuento: diasDescuento,
+        Tipo: factura.Tipo,
+        Fecha: moment(factura.FechaFactura).format("DD/MM/YYYY"),
+        Vencimiento: moment(factura.FechaVencimiento).format("DD/MM/YYYY"),
+        FechaDescuento: fechaDescuento.isValid() ? fechaDescuento.format("DD/MM/YYYY") : "",
+        Valor: factura.TotalFactura.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
+        Saldo: factura.Saldo.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')     
+      }
+      return facturatmp;
+  }
+
+  const ProcesarCuota = (cuota,acuerdo)=>{
+    let dias = moment(cuota.FechaVencimiento).diff(moment(new Date()), 'days')
+    let diasDescuento = 0;
+    let fechaDescuento = moment(cuota.FechaMaxDescuento);
+    if (fechaDescuento.isValid()) {
+      diasDescuento = moment(cuota.FechaMaxDescuento).diff(moment(new Date()), 'days');
+    }
+
+    let cuotatmp={
+      Factura: cuota.Factura,
+      Acuerdo: acuerdo,
+      Dias: dias,
+      DiasDescuento: diasDescuento,
+      Cuota: cuota.NumeroCuota,
+      Fecha: moment(cuota.FechaFactura).format("DD/MM/YYYY"),
+      Vencimiento: moment(cuota.FechaVencimiento).format("DD/MM/YYYY"),
+      FechaDescuento: fechaDescuento.isValid() ? fechaDescuento.format("DD/MM/YYYY") : "",
+      Valor: cuota.ValorCuota.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
+      Saldo: cuota.Saldo.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')     
+    }
+    return cuotatmp;
+}
+
+  const OpenModalVencido = (acuerdo)=>{
+    const acuerdoFiltrado = props.AcuerdosXTipoPedido.filter(x=>x.TipoPedido===acuerdo);
+    const facturasEnAcuerdo = acuerdoFiltrado[0].Acuerdos;
+
+    let facturas = [];
+
+    if(acuerdo==='Precompra')
+    {
+        facturasEnAcuerdo.forEach(e=>{          
+          e.Facturas.forEach(factura=>{
+    
+            const fechaVencimiento = new Date(factura.FechaVencimiento);
+            const fechaActual      = new Date();
+            const isVencida        = fechaActual>fechaVencimiento;
+    
+            if(isVencida){
+              facturas.push(ProcesarFactura(factura,acuerdo,e.Acuerdo));  
+            }    
+          });
+        });
+
+      setDataModalPrecompra(facturas);
+      setOpenModalPrecompra(true);
+    }else{
+      facturasEnAcuerdo.forEach(e=>{
+        e.Facturas.forEach(factura=>{
+  
+          const fechaVencimiento = new Date(factura.FechaVencimiento);
+          const fechaActual      = new Date();
+          const isVencida        = fechaActual>fechaVencimiento;
+  
+          if(isVencida){
+            facturas.push(ProcesarFactura(factura,acuerdo,e.Acuerdo));
+        }
+  
+        });
+      });
+  
+      setDataModal(facturas);
+      setOpenModal(true);
+    }
+  }
+
+  const OpenModalAVencer = (acuerdo)=>{
+
+    const acuerdoFiltrado   = props.AcuerdosXTipoPedido.filter(x=>x.TipoPedido===acuerdo);
+    const facturasEnAcuerdo = acuerdoFiltrado[0].Acuerdos;
+
+    let facturas = [];
+
+    if(acuerdo==='Precompra'){
+      facturasEnAcuerdo.forEach(e=>{
+        e.Facturas.forEach(factura=>{
+
+          let diasParaVencer   = moment(factura.FechaVencimiento).diff(moment(new Date()), 'days');
+          let isFacturaAVencer = (Math.sign(diasParaVencer)===1) && diasParaVencer<=15;
+
+          if(isFacturaAVencer){
+            facturas.push(ProcesarFactura(factura,acuerdo,e.Acuerdo));
+          } 
+        });
+      });
+
+      setDataModalPrecompra(facturas);
+      setOpenModalPrecompra(true);
+    }else{
+        facturasEnAcuerdo.forEach(e=>{
+          e.Facturas.forEach(factura=>{
+
+            let diasParaVencer   = moment(factura.FechaVencimiento).diff(moment(new Date()), 'days');
+            let isFacturaAVencer = (Math.sign(diasParaVencer)===1) && diasParaVencer<=15;
+
+            if(isFacturaAVencer){
+              facturas.push(ProcesarFactura(factura,acuerdo,e.Acuerdo));
+            } 
+          });
+        });
+
+        setDataModal(facturas);
+        setOpenModal(true);
+    }
+  }
+
   props.AcuerdosXTipoPedido.forEach(acuXTipPed => {
     let saldo = 0;
     let saldoVencido = 0;
     let saldo15DiasAvencer = 0;
     let disponible = 0;
     acuXTipPed.Acuerdos.forEach(acu => {
-      if (acuXTipPed.AgrupaPorCuota) {
-        disponible += Number(acu.Disponible);
-      } else {
-        disponible = (props.Cliente.CreditoDisponible ? props.Cliente.CreditoDisponible : 0);
+      if(acuXTipPed.AgrupaPorCuota){
+        disponible += Number(acu.Disponible); 
+      }else{
+        disponible = (props.Cliente.CreditoDisponible?props.Cliente.CreditoDisponible:0);
       }
       acu.Facturas.forEach(fact => {
         fact.Cuotas.forEach(cuot => {
@@ -122,8 +357,8 @@ const FacturaTable = props => {
       Tipo: acuXTipPed.TipoPedido,
       Disponible: Number(disponible).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
       SaldoTotal: saldo.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
-      C15Dias: saldo15DiasAvencer.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
-      Vencido: saldoVencido.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'),
+      C15Dias: (<div><p style={{display:"inline-block"}}>{saldo15DiasAvencer.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</p>{(saldo15DiasAvencer>0)?<FaEye onClick={()=>{OpenModalAVencer(acuXTipPed.TipoPedido)}} size={"20px"} style={{display:"inline-block",marginLeft:'10px'}}/>:<span style={{display:"inline-block"}}></span>}</div>),
+      Vencido: (<div><p style={{display:"inline-block"}}>{saldoVencido.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}</p>{(saldoVencido>0)?<FaEye onClick={()=>{OpenModalVencido(acuXTipPed.TipoPedido)}} size={"20px"} style={{display:"inline-block",marginLeft:'10px'}}/>:<span style={{display:"inline-block"}}></span>}</div>),
       Accion: (<Button
         onClick={() => { setCuotas(acuXTipPed.TipoPedido) }}
         variant="contained"
@@ -132,6 +367,7 @@ const FacturaTable = props => {
     </Button>)
     })
   });
+
   data.push(
     {
       Tipo: (
@@ -144,7 +380,7 @@ const FacturaTable = props => {
         {Totales.SaldoTotal.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
       </label>),
       Vencido: (<label className="font-weight-bolder text-dark">
-        {Totales.Vencido.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+        {Totales.Vencido.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}     
       </label>),
       C15Dias: (<label className="font-weight-bolder text-dark">
         {Totales.C15Dias.toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
@@ -152,22 +388,13 @@ const FacturaTable = props => {
       Accion: (<label ></label>)
     }
   );
+
   return (
-
-    <MuiThemeProvider theme={getMuiTheme()}>
-      <MUIDataTable title={'Resumen Cartera'} data={data} columns={columns} options={getOptions(props)} />
-    </MuiThemeProvider>
-  )
+  <>
+    <MUIDataTable title={'Resumen Cartera'} data={data} columns={columns} options={getOptions(props)} />
+    <FacturasModal Data={DataModal} Open={openModal} onClose={setOpenModal}></FacturasModal>
+    <FacturasModalPrecompra Data={DataModalPrecompra} Open={openModalPrecompra} onClose={setOpenModalPrecompra}></FacturasModalPrecompra>
+    <CuotaModal  Data={cuotasa} Open={openModalCuota} onClose={setOpenModalCuota}/>
+  </>)
 }
-
-const getMuiTheme = () => createMuiTheme({
-  overrides: {
-    MUIDataTable: {
-      responsiveScrollMaxHeight: {
-        maxHeight: 'unset !important',
-      }
-    },
-  }
-});
-
 export default FacturaTable
---
+++ src/components/Recibos/FacturasModal/FacturasModalPrecompra.js
@@ -0,0 +1,189 @@
+import React from "react";
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@material-ui/core';
+import MUIDataTable from 'mui-datatables'
+
+
+const FacturasModalPrecompra = (props) => {
+
+    const columns = [
+
+        {
+            name: 'Tipo',
+            label: 'Tipo',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'NumeroFactura',
+            label: 'Numero Factura',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'Fecha',
+            label: 'Fecha',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'Vencimiento',
+            label: 'Vencimiento',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+
+        {
+            name: 'Dias',
+            label: 'Dias',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'FechaDescuento',
+            label: 'Fecha Descuento',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'DiasDescuento',
+            label: 'Dias Descuento',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'Valor',
+            label: 'Valor',
+            options: {
+                filter: true,
+                sort: false
+            }
+        },
+        {
+            name: 'Saldo',
+            label: 'Saldo',
+            options: {
+                filter: true,
+                sort: true
+            }
+        },
+        {
+            name: 'Cuotas',
+            label: 'Cuotas',
+            options: {
+                filter: false,
+                sort: false
+            }
+        }
+    ]
+
+
+    const options = {
+        filterType: 'false',
+        responsive: "scrollFullHeight",
+        print: false,
+        download: false,
+        pagination: false,
+        sortFilterList: false,
+        filter: false,
+        search: false,
+        viewColumns: false,
+        selectableRows: 'none',
+        // rowsSelected: selectedRowsIndex,
+        textLabels: {
+            body: {
+                noMatch: "No se han encontrado pedidos",
+                toolTip: "Ordenar",
+            },
+            pagination: {
+                next: "Siguiente",
+                previous: "Anterior",
+                rowsPerPage: "Filas por página:",
+                displayRows: "de",
+            },
+            toolbar: {
+                search: "Buscar",
+                downloadCsv: "Descargar CSV",
+                print: "Imprimir",
+                viewColumns: "Ver Columnas",
+                filterTable: "Filtrar Tabla",
+            },
+            filter: {
+                all: "Todos",
+                title: "Filtros",
+                reset: "Quitar",
+            },
+            viewColumns: {
+                title: "Mostrar Columnas",
+                titleAria: "Mostrar/Esconder Columnas",
+            },
+            selectedRows: {
+                text: "Fila(s) seleccionadas",
+                delete: "Borrar",
+                deleteAria: "Borrar Filas Seleccionadas",
+            }
+        },
+        // onRowsSelect: (currentRowsSelected, allRowsSelected) => {
+        //     setSelectedRowsIndex(allRowsSelected.map(row => row.dataIndex))
+        // }
+    }
+
+    return (
+        <>
+            <Dialog
+                scroll={'paper'}
+                open={props.Open}
+                onClose={() => props.onClose(false)}>
+                <DialogTitle className="text-center" id="scroll-dialog-title">
+                    <div style={{ fontWeight: 300, fontSize: '24px', fontFamily: 'Poppins, Roboto, "Helvetica Neue", Arial, sans-serif' }}>
+                        Facturas
+                </div>
+                </DialogTitle>
+                <DialogContent>
+                    <MUIDataTable
+                        title={''}
+                        data={props.Data}
+                        columns={columns}
+                        options={options}
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="outlined" onClick={() => props.onClose(false)} color="primary">
+                        Cancelar
+                </Button>
+                    {/* <Button
+                    variant="outlined"
+                    color="primary"
+                    className={"py-1"}
+                    style={{ height: '35px' }}
+                    disabled={!this.state.razonSelected || (causaDisabled && tipoDisabled)}
+                    onClick={() => this.guardarRazon()}>
+                    {
+                        this.state.guardandoRazon ?
+                            <ScaleLoader
+                                css={{ height: '25px', bottom: '5px', position: 'relative', transform: 'scale(0.6)' }}
+                                size={'20px'}
+                                color={'#3f51b5'}
+                                loading={this.state.GuardarAsignacion} /> : 'Guardar'
+                    }
+                </Button> */}
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
+
+export default FacturasModalPrecompra;
