24d7e9d280b1fe02ed8bf9c9087a658fd3cb24a5
Francisco A. Garcia <frangarcia38@hotmail.com>
2020-08-27 17:51:04 -0600

    Commit con los siguientes cambios: - Link de api apuntando al servidor de tigo

--- src/utils/Enviroment.js
+++ src/utils/Enviroment.js
@@ -1,3 +1,3 @@
 // export const APIURL = 'https://aventas.devcit.com:3044';
-export const APIURL = 'http://190.109.203.183:9080';
-export const APIKEY = 'AIzaSyDDDBTrXW70m9SkdgHjwWDwJcewVTT0CzY';
+export const APIURL = 'https://crm.intermoda.hn:41443';
+export const APIKEY = 'AIzaSyCpnDhF1L7k1MJdu7UJLElTUnlAhFMi1_0';
