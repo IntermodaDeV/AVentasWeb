38807d2801b404e716b6f2d58ac773c44b62dbf5
Carlos Bueso <cbueso@intermoda.com.hn>
2022-10-18 14:02:23 -0600

    obtencion de productos

--- src/components/RecolocacionPedido/SelectCliente/Recolocacion.js
+++ src/components/RecolocacionPedido/SelectCliente/Recolocacion.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {
     Card,
     CardContent,
 } from '@material-ui/core';
+import axios from 'axios';
+import { APIURL } from 'utils/Enviroment';
+
 
 export const Recolocacion = (props) => {
+
+    const [rma, setRma] = useState("");
+    const [productos, setproductos ]  = useState([]);
+    const obtenerProductoRma = async () =>{
+        try{
+            const data = await axios.get(`${APIURL}/api/trasladopedido/obtenerproductos/${rma}`);
+            console.log(data.data)
+            setproductos(data.data);
+        }catch{
+            setproductos([]);
+        }
+
+    }
     return (
         <Card style={{ margin: '15px' }}>
             <CardContent>
@@ -12,9 +28,9 @@ export const Recolocacion = (props) => {
                     <h3>Recolocación de devolución</h3>
                     <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '100%', marginTop: 40 }}>
                         <div style={{ display: 'flex', width: '90%'}}>
-                            <input type="text" className="mr-5 form-control" placeholder="RMA Devolución" /*onChange={ (e) => { setCodigo(e.target.value) }}*/ />
+                            <input type="text" className="mr-5 form-control" placeholder="RMA Devolución" onChange={ (e) => { setRma(e.target.value) }} />
                         </div>
-                        <button className="btn btn-success" /*onClick={obtenerProductosFactura}*/>Registrar</button>
+                        <button className="btn btn-success" onClick={obtenerProductoRma}>Registrar</button>
                     </div>
                 </div>
             </CardContent>
