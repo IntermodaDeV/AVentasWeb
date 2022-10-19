14cd66c70b299de82f072d77c3fb6bfea2d84638
lcaceres0022 <lucy_alcantara2000@hotmail.com>
2022-10-19 08:37:01 -0600

    -Mensaje al obtener productos del RMA

--- src/components/RecolocacionPedido/SelectCliente/Recolocacion.js
+++ src/components/RecolocacionPedido/SelectCliente/Recolocacion.js
@@ -1,39 +1,51 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import {
     Card,
     CardContent,
 } from '@material-ui/core';
 import axios from 'axios';
 import { APIURL } from 'utils/Enviroment';
+import { Loading } from 'components/Global/Loading';
 
 
 export const Recolocacion = (props) => {
 
     const [rma, setRma] = useState("");
-    const [productos, setproductos ]  = useState([]);
-    const obtenerProductoRma = async () =>{
-        try{
+    const [productos, setproductos] = useState([]);
+    const [title, setTitle] = useState("");
+    const [open, setOpen] = useState(false);
+    const obtenerProductoRma = async () => {
+        try {
+            setTitle("Obteniendo productos de la devolución");
+            setOpen(true);
+            setRma("RM-0023971")
             const data = await axios.get(`${APIURL}/api/trasladopedido/obtenerproductos/${rma}`);
-            console.log(data.data)
             setproductos(data.data);
-        }catch{
+            //agregarDevolucionCompleta(productosDevolver, data.data);
+            setOpen(false);
+        } catch {
             setproductos([]);
         }
 
     }
+
     return (
-        <Card style={{ margin: '15px' }}>
-            <CardContent>
-                <div style={{ marginTop: 20 }}>
-                    <h3>Recolocación de devolución</h3>
-                    <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '100%', marginTop: 40 }}>
-                        <div style={{ display: 'flex', width: '90%'}}>
-                            <input type="text" className="mr-5 form-control" placeholder="RMA Devolución" onChange={ (e) => { setRma(e.target.value) }} />
+        <>
+            <Loading open={open} title={title} />
+            <Card style={{ margin: '15px' }}>
+                <CardContent>
+                    <div style={{ marginTop: 20 }}>
+                        <h3 style={{ marginLeft: 30 }}>Recolocación de devolución</h3>
+                        <div style={{ display: 'flex', justifyContent: 'space-evenly', width: '100%', marginTop: 40 }}>
+                            <div style={{ display: 'flex', width: '90%' }}>
+                                <input type="text" className="mr-5 form-control" placeholder="RMA Devolución" onChange={(e) => { setRma(e.target.value) }} />
+                            </div>
+                            <button className="btn btn-success" onClick={obtenerProductoRma}>Registrar</button>
                         </div>
-                        <button className="btn btn-success" onClick={obtenerProductoRma}>Registrar</button>
                     </div>
-                </div>
-            </CardContent>
-        </Card>
+
+                </CardContent>
+            </Card>
+        </>
     );
 }
